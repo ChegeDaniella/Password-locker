6b3220829ebcbab28896402e7d6bdc087e1ade79
ChegeDaniella <chegedaniella@gmail.com>
2020-04-19 10:17:00 +0300

    function main working

--- run.py
+++ run.py
@@ -15,51 +15,55 @@ def save_users(user):
     User.save_user()
 
 def main():
-    print("Hello welcome to creating a new account")
-    user_name = input()
+        print("Hello welcome to creating a new account.What iis your name?")
+        user_name = input()
 
-    print(f"Hello{user_name}.Chose a path to take increating yur passwords")
-    print('\n')
+        print(f"Hello{user_name}.Chose a path to take increating or opening your account")
+        print('\n')
 
-    while True:
-        print("Use this shortcuts : cu - create new account with your own password, dc - display") 
+        while True:
+            print("Use these shortcuts: oc - to open a new account, dc - diplay accounts")
+            path = input()
 
-        short_code = input().lower()
-        if short_code == 'cu':
-            print("Please enter your prefered interface...")
-            Username = input() 
+            # if path == 'oc':
+                
+            #     print("Use this shortcuts : cu - create new account with your own password, dc - display") 
 
-            print("Use this short cuts to chose how to create a pass word.cp - create your own password ")  
-            
-            password = input().lower()
-        if password == 'cp':
-            print("Create your own password.")
-        elif password == 'rp':
-            password = random.randint(0,50)    
-        else: 
-                print("Please enter password.")
+            #     short_code = input().lower()
+            # if short_code == 'cu':
+            #     print("Please enter your prefered username...")
+            #     Username = input() 
 
-                save_users(create_user(Username,Password))#create and save user   
-                print(f"New user account {Username} {password} has been created")     
+            #     print("Use this short cuts to chose how to create a pass word.cp - create your own password,rp - a password to be generated ")   
+            #     password = input().lower()
 
-
-
-
-
-
+            # if password == 'cp':
+            #     print("Create your own password.")
+            #     Password= input()
+            # elif password == 'rp':
+            #     password = random.randint(0,50)   
+            #     Password=input() 
+            # else: 
+            #         print("Please enter password.")
 
 
-            elif short_code == 'dc':
+            #         save_users(create_user(Username.password))#create and save user   
+            #         print(f"New user account {Username} {password} has been created")   
+            #         print('\n')  
 
-                if display_user():
-                    print("Here is al list")
-                    print('\n')
+            # elif short_code == 'dc':
 
-                    for User in display_user():
-                        print(f"{User.Username}")
+            #     if display_user():
+            #         print("Here is al list")
+            #         print('\n')
 
-                else:
-                    print("No user accounts found")        
+            #         for User in display_user():
+            #             print(f"{User.Username}")
+
+            #     else:
+            #         print("No user accounts found") 
+if __name__ == '__main__':
+            main()                    
 
 
 
